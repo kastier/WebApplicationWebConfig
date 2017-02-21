a7915523fabff812e2ccf2a8d8928a1631b734ec
Kastier Y <kastier@outlook.com>
2017-02-21 16:26:30 +0800

    1`2`12

--- ClassLibraryV2/CacheHelper.cs
+++ ClassLibraryV2/CacheHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Security;
@@ -11,29 +12,8 @@ namespace ClassLibraryV2
     {
         public static string getTickekInfoFormCacheString(string ticketStr)
         {
-            
-            if (string.IsNullOrEmpty(ticketStr))
-            {
-
-                return "{\"Status\":1}";
-            }
-            else
-            {
-                try
-                {
-                    
-                    var ticket = FormsAuthentication.Decrypt(ticketStr);
-                    string userData = ticket.UserData;
-                    string userShowName = userData.Substring(userData.IndexOf("ShowName") + 11, userData.LastIndexOf("}") - userData.IndexOf("ShowName") - 12);
-                    return  $"{{ \"Status\":0,  \"UserSessionInfo\": {{\"UserName\" :\"{ticket.Name}\",\"UserShowName\" : \"{userShowName}\",\"AuthencatedTime\" : \"{ticket.Expiration}\"}}}}";
-                    
-                }
-                catch (Exception exp)
-                {
-                    return "{\"Status\":1}";
-                }
-
-            }
+            HttpClient hc = new HttpClient();
+            return "error";
         }
     }
 }
